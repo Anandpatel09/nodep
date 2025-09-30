b39a91df37e88b66108024585b1eed7c26751e1d
Anandpatel09 <anandbca2021@gmail.com>
2025-09-30 22:33:40 +0530

    exploring get post delete use,

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 const express =require("express");
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the home")
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Anand", lastName:"Patel"})
 })
-app.use("/test",(req,res)=>{
+
+app.post("/user",(req,res)=>{
+    //logic of saving data to data base
+    res.send("Data saved to database successfully")
+})
+
+
+app.delete("/user",(req,res)=>{
+res.send("data deleted successfully")
+})
+
+
+app.use("/user",(req,res)=>{
     res.send("Hello from the server")
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello")
-})
+// app.use("/hello",(req,res)=>{
+//     res.send("hello hello hello")
+// })
 
-app.use("/world",(req,res)=>{
-    res.send("hello hello hello")
-})
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the home")
+// })
+
 app.listen(3000 , ()=>{
     console.log("Server is runing on port 3000...")
 });
